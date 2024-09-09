4bc376effda297487826111338039be629ad4678
Dpratik015 <136247031+Dpratik015@users.noreply.github.com>
2024-09-09 13:01:00 +0530

    Update vars.tf with new form data

--- vars.tf
+++ vars.tf
@@ -1,4 +1,4 @@
-"host_name" = "ABCD123",
+"host_name" = "SCFGHJ1234",
 "image_id" = data.openstack_images_image_v2.ubuntu_image.id,
 "flavor_id" = data.openstack_compute_flavor_v2.8G_32G,
-"secondary_disk_size" = 100
+"secondary_disk_size" = 200
