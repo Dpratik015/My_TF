9d486ac0e795f5dc9368e87f5941145c7c2ae493
Dpratik015 <136247031+Dpratik015@users.noreply.github.com>
2024-09-10 16:54:09 +0530

    Append new data to vars.tf

--- vars.tf
+++ vars.tf
@@ -63,6 +63,7 @@ locals {
  { "host_name" = "ABCD1234","image_id" = data.openstack_images_image_v2.ubuntu_image.id,"flavor_id" = data.openstack_compute_flavor_v2.8G_32G,"secondary_disk_size" = 46 }
 
  { "host_name" = "ABCD1234","image_id" = data.openstack_images_image_v2.ubuntu_image.id,"flavor_id" = data.openstack_compute_flavor_v2.8G_32G,"secondary_disk_size" = 46 }
+{ "host_name" = "ABCD123","image_id" = data.openstack_images_image_v2.ubuntu_image.id,"flavor_id" = data.openstack_compute_flavor_v2.8G_32G,"secondary_disk_size" = 23 }
 ]
 
 }
