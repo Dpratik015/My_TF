4a051b314ced9cae3ef52a3424d6bb67af80dc66
Dpratik015 <136247031+Dpratik015@users.noreply.github.com>
2024-09-11 12:36:55 +0530

    Append new VM details to vars.tf

--- vars.tf
+++ vars.tf
@@ -68,6 +68,10 @@ locals {
 
 { "host_name" = "ASDFGHJK123","image_id" = data.openstack_images_image_v2.ubuntu_image.id ,"flavor_id" = data.openstack_compute_flavor_v2.8G_32G, "secondary_disk_size" = 66 },
 
+{ "host_name" = "ABC123","image_id" = data.openstack_images_image_v2.ubuntu_image.id ,"flavor_id" = data.openstack_compute_flavor_v2.4g_32, "secondary_disk_size" = 50 },
+{ "host_name" = "DEF123","image_id" = data.openstack_images_image_v2.ubuntu_image.id ,"flavor_id" = data.openstack_compute_flavor_v2.4g_32, "secondary_disk_size" = 50 },
+{ "host_name" = "GHI123","image_id" = data.openstack_images_image_v2.ubuntu_image.id ,"flavor_id" = data.openstack_compute_flavor_v2.4g_32, "secondary_disk_size" = 50 },
+
 ]
 
 }
